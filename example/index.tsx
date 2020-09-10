c8075e62f7e48c52078581f0cd20e0094204c920
Aryella <aryella.lacerda@gmail.com>
2020-09-10 15:47:03 -0300

    chore: install optimization-related tools

--- example/index.tsx
+++ example/index.tsx
@@ -1,5 +1,13 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+if (process.env.NODE_ENV === 'development') {
+  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  whyDidYouRender(React, {
+    // trackAllPureComponents: true,
+  });
+}
