e3af720e860b90716ff5a4c2628a84ca2a35314b
--get-all <aryella.lacerda@gmail.com>
2020-09-23 14:03:12 -0300

    refactor: remove unused Day entity

--- src/Entities/Day.ts
+++
@@ -1,6 +0,0 @@
-import type { DateProperties } from './DateProperties';
-
-export interface Day {
-  properties: DateProperties | null;
-  date: string;
-}
--- src/Entities/index.ts
+++ src/Entities/index.ts
@@ -1,6 +1,5 @@
 export * from './ArrowDirections';
 export * from './DateProperties';
-// export * from './Day';
 export * from './Theme';
 export * from './Locale';
 export * from './Components';
