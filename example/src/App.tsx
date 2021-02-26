8904e57903c54b4b012a4fde958cae9c0d23f2ed
--get-all <aryella.lacerda@superlogica.com>
2021-02-26 11:12:19 -0300

    chore: add testing screen to calendar

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,26 +6,35 @@ import Menu from './Menu';
 import SingleDateSelection from './Calendars/SingleDateSelection';
 import MultiDateSelection from './Calendars/MultiDateSelection';
 
+import DebugConfig from '../config';
+import TestingScreen from './TestingScreen';
+
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Menu" component={Menu} options={{ headerShown: false }} />
-        <Stack.Screen
-          name="DateSelectionExamples"
-          component={SingleDateSelection}
-          options={{ title: 'Date Selection Examples', headerBackTitleVisible: false }}
-        />
-        <Stack.Screen
-          name="MultiDateSelectionExamples"
-          component={MultiDateSelection}
-          options={{
-            title: 'Multi Date Selection Examples',
-            headerBackTitleVisible: false,
-          }}
-        />
+        {DebugConfig.runTestingScreen ? (
+          <Stack.Screen name="Testing" component={TestingScreen} />
+        ) : (
+          <>
+            <Stack.Screen name="Menu" component={Menu} options={{ headerShown: false }} />
+            <Stack.Screen
+              name="DateSelectionExamples"
+              component={SingleDateSelection}
+              options={{ title: 'Date Selection Examples', headerBackTitleVisible: false }}
+            />
+            <Stack.Screen
+              name="MultiDateSelectionExamples"
+              component={MultiDateSelection}
+              options={{
+                title: 'Multi Date Selection Examples',
+                headerBackTitleVisible: false,
+              }}
+            />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
